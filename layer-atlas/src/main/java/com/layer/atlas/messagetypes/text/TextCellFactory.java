3da9307da6db00767683bc4b8474e6c1c29c55be
Joseph Watkins <joseph.l.watkins@gmail.com>
2017-09-08 23:00:01 -0400

    UNDO - Had to hard set values because not working with styles

--- layer-atlas/src/main/java/com/layer/atlas/messagetypes/text/TextCellFactory.java
+++ layer-atlas/src/main/java/com/layer/atlas/messagetypes/text/TextCellFactory.java
@@ -3,6 +3,7 @@ package com.layer.atlas.messagetypes.text;
 import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.ContentLoadingProgressBar;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -41,13 +42,17 @@ public class TextCellFactory extends AtlasCellFactory<TextCellFactory.CellHolder
     @Override
     public CellHolder createCellHolder(ViewGroup cellView, boolean isMe, LayoutInflater layoutInflater) {
         View v = layoutInflater.inflate(R.layout.atlas_message_item_cell_text, cellView, true);
+
+        Context c = v.getContext();
+
+
         v.setBackgroundResource(isMe ? R.drawable.atlas_message_item_cell_me : R.drawable.atlas_message_item_cell_them);
-        ((GradientDrawable) v.getBackground()).setColor(isMe ? mMessageStyle.getMyBubbleColor() : mMessageStyle.getOtherBubbleColor());
+        ((GradientDrawable) v.getBackground()).setColor(isMe ? ContextCompat.getColor(c, R.color.atlas_text_blue) : ContextCompat.getColor(c, R.color.atlas_color_primary_gray));
 
         TextView t = (TextView) v.findViewById(R.id.cell_text);
-        t.setTextSize(TypedValue.COMPLEX_UNIT_PX, isMe ? mMessageStyle.getMyTextSize() : mMessageStyle.getOtherTextSize());
-        t.setTextColor(isMe ? mMessageStyle.getMyTextColor() : mMessageStyle.getOtherTextColor());
-        t.setLinkTextColor(isMe ? mMessageStyle.getMyTextColor() : mMessageStyle.getOtherTextColor());
+        t.setTextSize(TypedValue.COMPLEX_UNIT_PX, isMe ? mMessageStyle.getMyTextSize() + 4 : mMessageStyle.getOtherTextSize() + 4);
+        t.setTextColor(isMe ? ContextCompat.getColor(c, R.color.atlas_text_white) : ContextCompat.getColor(c, R.color.atlas_text_black));
+        t.setLinkTextColor(isMe ? ContextCompat.getColor(c, R.color.atlas_text_white) : ContextCompat.getColor(c, R.color.atlas_text_black));
         t.setTypeface(isMe ? mMessageStyle.getMyTextTypeface() : mMessageStyle.getOtherTextTypeface(), isMe ? mMessageStyle.getMyTextStyle() : mMessageStyle.getOtherTextStyle());
         return new CellHolder(v);
     }
